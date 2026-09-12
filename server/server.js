ac3736c8c28da6cfa12a42f688c6657162268a31
Shreejal Pradhan <pradhanshreejal0@gmail.com>
2026-09-12 20:29:08 +0545

    Configure production CORS

--- server/server.js
+++ server/server.js
@@ -24,8 +24,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
+    origin: [
+          "http://localhost:5173",
+          "https://finalecommercewebsite.vercel.app",
+        ],
+        credentials: true,
   })
 );
 app.use(express.json());
