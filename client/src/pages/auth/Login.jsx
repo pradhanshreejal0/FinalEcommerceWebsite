0d30e1b7c4293f6f860e7de77c2ec7c5d8296f33
pradhanshreejal0 <pradhanshreejal0@gmail.com>
2026-09-17 22:09:31 -0600

    making forget and reset password

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -64,8 +64,8 @@ export default function Login() {
             </div>
 
             <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
               <div className="flex items-center justify-between">
-                             <Label htmlFor="password">Password</Label>
 
               <div className="relative">
                 <Input
