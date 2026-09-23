875715d3c6fc4c81cb9f146e68f0437e09f42774
pradhanshreejal0 <pradhanshreejal0@gmail.com>
2026-09-23 09:12:27 -0600

    fixing indexing 3

--- client/src/pages/customer/Checkout.jsx
+++ client/src/pages/customer/Checkout.jsx
@@ -746,7 +746,7 @@ export default function Checkout() {
                 </div>
 
                 {/* SEARCH + AUTO LOCATE */}
-               <div className="relative z-10">
+               <div className="relative z-20">
                   <div className="flex flex-col gap-2 sm:flex-row">
                     <div className="relative flex-1">
                       <Search className="pointer-events-none absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
